d144bdada52e0ae9f816b9d765d18f24f3067c30
wilson <jwmrf@cin.ufpe.br>
2023-04-10 16:29:45 -0300

    Cors + Concurrently [BACK] #master

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import Cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Teste FullStack Garupa')
     .setDescription('Back-end do teste full stack garupa')
